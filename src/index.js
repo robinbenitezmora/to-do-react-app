d2efe37bfff0a8ae909d204bead27915044aca6a
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-12 11:36:55 -0500

    Update 'TodoContainer.js' file agree with tutorial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//component file
+import TodoContainer from './components/TodoContainer';
 
-const element = <h1>Hello from Creat React</h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
 
 
 // import React from 'react';
