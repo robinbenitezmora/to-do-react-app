2cbd27441c08def9d7e0bedf4c9824de8e54e973
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-12 15:48:31 -0500

    Add code lines in order to complete the 'form' structure

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
+import { v4 as uuidv4 } from 'uuid';
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false
       }
@@ -46,7 +47,18 @@ class TodoContainer extends React.Component {
       ]
     });
   };
-  
+
+  addTodoItem = title => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
   render() {
     return (
       <div>
