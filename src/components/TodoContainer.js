52d0f5688c6e52c0850433aa0f96de8f49518ee6
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-12 16:00:17 -0500

    Adjust code lines in 'TodoContainer.js'

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
+import InputTodo from './InputTodo';
 import { v4 as uuidv4 } from 'uuid';
 
 class TodoContainer extends React.Component {
@@ -50,7 +51,7 @@ class TodoContainer extends React.Component {
 
   addTodoItem = title => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title: title,
       completed: false
     };
